1a77ff956a64f7dc9ef2ce6c31c661ebda4ba158
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-05-05 21:37:58 +0200

    Added time benchmark to main

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
+	"time"
 
 	"github.com/AJMBrands/SoftwareThatMatters/ingest"
 )
@@ -15,11 +17,15 @@ const outPathTemplate string = "data/out/parsed_data_%d.csv"
 const versionPath string = "data/out/versions.csv"
 
 var m1, m2 runtime.MemStats
+var t1, t2 time.Time
 
 //TODO: Make ingest process and file writing scalable
 func main() {
 	runtime.ReadMemStats(&m1) // Reading memory stats for debug purposes
+	t1 = time.Now()
 	//ingest.IngestFile(offline_file, outPath)
 	ingest.Ingest(limited_discovery_query, outPathTemplate)
 	runtime.ReadMemStats(&m2)
+	t2 = time.Now()
+	fmt.Printf("Took %d ms", t2.UnixMilli()-t1.UnixMilli())
 }
