72341400172e4c1ebded105672f4b39ed1673c43
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-05-06 15:55:36 +0200

    Use StreamParse and MergeJSON in main

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ const limited_discovery_query string = "https://libraries.io/api/search?api_key=
 const discovery_query string = "https://libraries.io/api/search?api_key=3dc75447d3681ffc2d17517265765d23&platforms=NPM&per_page=20"
 
 const offline_file string = "data/100packages.json"
-const outPathTemplate string = "data/out/parsed_data_%d.csv"
 const versionPath string = "data/out/versions.csv"
+const outPathTemplate string = "data/out/streamedout-%s.json"
 
 var m1, m2 runtime.MemStats
 var t1, t2 time.Time
@@ -24,7 +24,10 @@ func main() {
 	runtime.ReadMemStats(&m1) // Reading memory stats for debug purposes
 	t1 = time.Now()
 	//ingest.IngestFile(offline_file, outPath)
-	ingest.Ingest(limited_discovery_query, outPathTemplate)
+	// ingest.Ingest(limited_discovery_query, outPathTemplate, versionPath)
+
+	amount := ingest.StreamParse("data/10k.rows.json", outPathTemplate)
+	ingest.MergeJSON(outPathTemplate, amount)
 	runtime.ReadMemStats(&m2)
 	t2 = time.Now()
 	fmt.Printf("Took %d ms", t2.UnixMilli()-t1.UnixMilli())
