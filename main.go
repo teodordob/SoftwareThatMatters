047a6b65d101712a41cacb7c671c4a91c06b3fab
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-05-10 12:27:24 +0200

    Reduced clutter in main.go

--- main.go
+++ main.go
@@ -8,12 +8,8 @@ import (
 	"github.com/AJMBrands/SoftwareThatMatters/ingest"
 )
 
-const limited_discovery_query string = "https://libraries.io/api/search?api_key=3dc75447d3681ffc2d17517265765d23&page=1&per_page=1&platforms=NPM"
+const inPath string = "data/input.json"
 
-const discovery_query string = "https://libraries.io/api/search?api_key=3dc75447d3681ffc2d17517265765d23&platforms=NPM&per_page=20"
-
-const offline_file string = "data/100packages.json"
-const versionPath string = "data/out/versions.csv"
 const outPathTemplate string = "data/out/streamedout-%s.json"
 
 var m1, m2 runtime.MemStats
@@ -26,7 +22,7 @@ func main() {
 	//ingest.IngestFile(offline_file, outPath)
 	// ingest.Ingest(limited_discovery_query, outPathTemplate, versionPath)
 
-	amount := ingest.StreamParse("data/input.json", outPathTemplate)
+	amount := ingest.StreamParse(inPath, outPathTemplate)
 	ingest.MergeJSON(outPathTemplate, amount)
 	runtime.ReadMemStats(&m2)
 	t2 = time.Now()
