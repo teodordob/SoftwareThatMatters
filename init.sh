0bb40a294c26c34acf16589c42e98ae0144491a8
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-05-18 13:48:59 +0200

    Tweaked init script

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-OUTFILE="data/input.json"
+OUTFILE="data/in/input.json"
 N=$1
 URL=""
 
@@ -9,7 +9,7 @@ mkdir data && mkdir data/out
 fi
 
 # If the argument is empty or negative, just download all records
-if [ -z "$N" ] || [ $N -lt 0 ]
+if [ -z "$N" ] || [ "$N" -lt 0 ]
 then
 URL="https://replicate.npmjs.com/_all_docs?include_docs=true"
 else # If N was positive and specified, download that many records
@@ -17,4 +17,4 @@ URL="https://replicate.npmjs.com/_all_docs?include_docs=true&limit=${N}"
 fi
 # The following script downloads the npm data dump and then projects so that we only have the rows entries
 echo "Starting download..."
-curl $URL > $OUTFILE && echo "Starting preprocessing..." && sed --regexp-extended -i 's/\{"total_rows":.*,"offset":.*,"rows":\[/[/;s/\]}$/\]/' $OUTFILE && echo "Done!"
+curl "$URL" > $OUTFILE && echo "Starting preprocessing..." && sed --regexp-extended -i 's/\{"total_rows":.*,"offset":.*,"rows":\[/[/;s/\]}$/\]/' $OUTFILE && echo "Done!"
