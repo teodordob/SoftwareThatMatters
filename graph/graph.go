1a9f55aa615dac36b04bc403917879fee13df942
Andrei Purcaru <a.purcaru@student.tudelft.nl>
2022-05-24 19:24:23 +0200

    Replace the manual Node with simple.Node

--- graph/graph.go
+++ graph/graph.go
@@ -4,20 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	semver2 "github.com/blang/semver/v4"
+	"gonum.org/v1/gonum/graph/encoding/dot"
+	"gonum.org/v1/gonum/graph/simple"
 	"log"
 	"os"
-
-	"gonum.org/v1/gonum/graph"
-	"gonum.org/v1/gonum/graph/encoding/dot"
-	"gonum.org/v1/gonum/graph/iterator"
-	"gonum.org/v1/gonum/graph/simple"
 )
-
-// GraphNode is a node in an implicit graph.
-type GraphNode struct {
-	id        int64
-	Neighbors []graph.Node
-}
 
 type VersionInfo struct {
 	Timestamp    string            `json:"timestamp"`
@@ -46,203 +37,6 @@ func NodeInfo(name, version, timestamp string) *nodeInfo {
 		Version:   version,
 		Timestamp: timestamp,
 	}
-}
-
-// NewGraphNode returns a new GraphNode.
-func NewGraphNode(id int64) *GraphNode {
-	return &GraphNode{id: id}
-}
-
-// Node allows GraphNode to satisfy the graph.Graph interface.
-func (g *GraphNode) Node(id int64) graph.Node {
-	if id == g.id {
-		return g
-	}
-
-	seen := map[int64]struct{}{g.id: {}}
-
-	for _, n := range g.Neighbors {
-		if n.ID() == id {
-			return n
-		}
-
-		if gn, ok := n.(*GraphNode); ok {
-			if gn.Has(seen, id) {
-				return gn
-			}
-		}
-	}
-
-	return nil
-}
-
-func (g *GraphNode) Has(seen map[int64]struct{}, id int64) bool {
-
-	for _, n := range g.Neighbors {
-		if _, ok := seen[n.ID()]; ok {
-			continue
-		}
-
-		seen[n.ID()] = struct{}{}
-		if n.ID() == id {
-			return true
-		}
-
-		if gn, ok := n.(*GraphNode); ok {
-			if gn.Has(seen, id) {
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-// Nodes allows GraphNode to satisfy the graph.Graph interface.
-func (g *GraphNode) Nodes() graph.Nodes {
-	nodes := []graph.Node{g}
-	seen := map[int64]struct{}{g.id: {}}
-
-	for _, n := range g.Neighbors {
-		nodes = append(nodes, n)
-		seen[n.ID()] = struct{}{}
-
-		if gn, ok := n.(*GraphNode); ok {
-			nodes = gn.nodes(nodes, seen)
-		}
-	}
-
-	return iterator.NewOrderedNodes(nodes)
-}
-
-func (g *GraphNode) nodes(dst []graph.Node, seen map[int64]struct{}) []graph.Node {
-
-	for _, n := range g.Neighbors {
-		if _, ok := seen[n.ID()]; ok {
-			continue
-		}
-
-		dst = append(dst, n)
-		if gn, ok := n.(*GraphNode); ok {
-			dst = gn.nodes(dst, seen)
-		}
-	}
-
-	return dst
-}
-
-// From allows GraphNode to satisfy the graph.Graph interface.
-func (g *GraphNode) From(id int64) graph.Nodes {
-	if id == g.ID() {
-		return iterator.NewOrderedNodes(g.Neighbors)
-	}
-
-	seen := map[int64]struct{}{g.id: {}}
-
-	for _, n := range g.Neighbors {
-		seen[n.ID()] = struct{}{}
-
-		if gn, ok := n.(*GraphNode); ok {
-			if result := gn.FindNeighbors(id, seen); result != nil {
-				return iterator.NewOrderedNodes(result)
-			}
-		}
-	}
-
-	return nil
-}
-
-func (g *GraphNode) FindNeighbors(id int64, seen map[int64]struct{}) []graph.Node {
-	if id == g.ID() {
-		return g.Neighbors
-	}
-
-	for _, n := range g.Neighbors {
-		if _, ok := seen[n.ID()]; ok {
-			continue
-		}
-		seen[n.ID()] = struct{}{}
-
-		if gn, ok := n.(*GraphNode); ok {
-			if result := gn.FindNeighbors(id, seen); result != nil {
-				return result
-			}
-		}
-	}
-
-	return nil
-}
-
-// HasEdgeBetween allows GraphNode to satisfy the graph.Graph interface.
-func (g *GraphNode) HasEdgeBetween(uid, vid int64) bool {
-	return g.EdgeBetween(uid, vid) != nil
-}
-
-// Edge allows GraphNode to satisfy the graph.Graph interface.
-func (g *GraphNode) Edge(uid, vid int64) graph.Edge {
-	return g.EdgeBetween(uid, vid)
-}
-
-// EdgeBetween allows GraphNode to satisfy the graph.Graph interface.
-func (g *GraphNode) EdgeBetween(uid, vid int64) graph.Edge {
-	if uid == g.id || vid == g.id {
-		for _, n := range g.Neighbors {
-			if n.ID() == uid || n.ID() == vid {
-				return simple.Edge{F: g, T: n}
-			}
-
-		}
-		return nil
-	}
-
-	seen := map[int64]struct{}{g.id: {}}
-
-	for _, n := range g.Neighbors {
-		seen[n.ID()] = struct{}{}
-		if gn, ok := n.(*GraphNode); ok {
-			if result := gn.edgeBetween(uid, vid, seen); result != nil {
-				return result
-			}
-		}
-	}
-
-	return nil
-}
-
-func (g *GraphNode) edgeBetween(uid, vid int64, seen map[int64]struct{}) graph.Edge {
-	if uid == g.id || vid == g.id {
-		for _, n := range g.Neighbors {
-			if n.ID() == uid || n.ID() == vid {
-				return simple.Edge{F: g, T: n}
-			}
-		}
-		return nil
-	}
-
-	for _, n := range g.Neighbors {
-		if _, ok := seen[n.ID()]; ok {
-			continue
-		}
-
-		seen[n.ID()] = struct{}{}
-		if gn, ok := n.(*GraphNode); ok {
-			if result := gn.edgeBetween(uid, vid, seen); result != nil {
-				return result
-			}
-		}
-	}
-
-	return nil
-}
-
-// ID allows GraphNode to satisfy the graph.Node interface.
-func (g *GraphNode) ID() int64 {
-	return g.id
-}
-
-// AddMeighbor adds an edge between g and n.
-func (g *GraphNode) AddNeighbor(n *GraphNode) {
-	g.Neighbors = append(g.Neighbors, graph.Node(n))
 }
 
 func CreateMap(in *[]PackageInfo) *map[int64]nodeInfo {
@@ -294,7 +88,7 @@ func CreateGraph(inputMap *map[int64]nodeInfo) *simple.DirectedGraph {
 	m := *inputMap
 	graph := simple.NewDirectedGraph()
 	for x := range m {
-		graph.AddNode(NewGraphNode(x))
+		graph.AddNode(simple.Node(x))
 	}
 	return graph
 }
