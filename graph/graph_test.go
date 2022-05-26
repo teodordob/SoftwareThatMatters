5c58df95cc4510461020d6e19aa13a5c16ef8d85
Andrei Purcaru <a.purcaru@student.tudelft.nl>
2022-05-26 21:45:56 +0200

    Test edges with a medium complexity graph

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -55,3 +55,83 @@ func TestCreateEdgesBasicGraph(t *testing.T) {
 		}
 	})
 }
+func TestCreateEdgesMediumComplexityGraph(t *testing.T) {
+	packagesInfo := []PackageInfo{
+		{
+			Name: "B",
+			Versions: map[string]VersionInfo{
+				"1.0.0": {
+					Timestamp: "2021-04-22T20:15:37",
+					Dependencies: map[string]string{
+						"A": "> 0.9.0",
+						"C": "1.0.0",
+					},
+				},
+			},
+		},
+		{
+			Name: "C",
+			Versions: map[string]VersionInfo{
+				"1.0.0": {
+					Timestamp: "2022-04-22T20:13:34",
+					Dependencies: map[string]string{
+						"A": "< 1.0.0",
+					},
+				},
+			},
+		},
+		{
+			Name: "A",
+			Versions: map[string]VersionInfo{
+				"0.9.0": {
+					Timestamp:    "2020-04-01T20:15:37",
+					Dependencies: map[string]string{},
+				},
+				"1.0.0-rc.1": {
+					Timestamp:    "2020-05-01T20:15:37",
+					Dependencies: map[string]string{},
+				},
+				"1.0.0": {
+					Timestamp:    "2021-06-01T20:15:37",
+					Dependencies: map[string]string{},
+				},
+				"1.1.0": {
+					Timestamp:    "2021-07-01T20:15:37",
+					Dependencies: map[string]string{},
+				},
+			},
+		},
+	}
+
+	graph := simple.NewDirectedGraph()
+	stringIDToNodeInfo := *CreateStringIDToNodeInfoMap(&packagesInfo, graph)
+	nameToVersions := *CreateNameToVersionMap(&packagesInfo)
+	CreateEdges(graph, &packagesInfo, &stringIDToNodeInfo, &nameToVersions)
+	t.Run("Creates 6 edges when there are 6 possible dependencies", func(t *testing.T) {
+		if graph.Edges().Len() != 6 {
+			t.Errorf("Expected 6 edges, got %d", graph.Edges().Len())
+		}
+	})
+	t.Run("Creates edges to the correct dependencies for Node B-1.0.0", func(t *testing.T) {
+		if graph.From(stringIDToNodeInfo["B-1.0.0"].id).Len() != 4 {
+			t.Errorf("Expected 4 possible dependencies for Node B-1.0.0, got %d", graph.From(stringIDToNodeInfo["B-1.0.0"].id).Len())
+		}
+		nodesIterator := graph.From(stringIDToNodeInfo["B-1.0.0"].id)
+		counter := 0
+		for nodesIterator.Next() {
+			currentNode := nodesIterator.Node()
+			if currentNode.ID() == graph.Node(stringIDToNodeInfo["C-1.0.0"].id).ID() {
+				counter++
+			}
+		}
+		if counter > 1 {
+			t.Errorf("Expected exactly 1 edge towards dependency C-1.0.0, got %d", counter)
+		}
+
+	})
+	t.Run("Creates no edges from Node A-1.0.0 (it has no dependencies)", func(t *testing.T) {
+		if graph.From(stringIDToNodeInfo["A-1.0.0"].id).Len() != 0 {
+			t.Errorf("Expected 0 dependencies for Node A-1.0.0, got %d", graph.From(stringIDToNodeInfo["A-1.0.0"].id).Len())
+		}
+	})
+}
