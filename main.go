a05a832460408b2254dc2ccaf51d2f70b0f10502
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-06-01 20:22:26 +0200

    Revert "Tidied up main.go"
    This reverts commit 6bc7385be63c561f98e31bbc44cf4390bf1a7d73.
    
    Conflicts:
    	main.go

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/AJMBrands/SoftwareThatMatters/cmd"
 	g "github.com/AJMBrands/SoftwareThatMatters/graph"
 	"gonum.org/v1/gonum/graph"
-	"gonum.org/v1/gonum/graph/network"
 	"gonum.org/v1/gonum/graph/simple"
 	"gonum.org/v1/gonum/graph/traverse"
 )
 
 func main() {
+	// To use the cli: go run main.go start.
+	cmd.Execute()
 	parsed := g.ParseJSON("data/input/test_data.json")
 	graph1 := simple.NewDirectedGraph()
 	stringIDToNodeInfo := g.CreateStringIDToNodeInfoMap(parsed, graph1)
@@ -24,14 +24,6 @@ func main() {
 			return false
 		},
 	}
-	x := w.Walk(graph1, graph1.Node(0), nil)
-	pageranking := network.PageRank(graph1, 0.85, 0.00001)
-
-	fmt.Println(x)
-	fmt.Println(pageranking)
-	//Uncomment this to create the visualization and use these commands in the dot file
-	//Toggle Preview - ctrl+shift+v (Mac: cmd+shift+v)
-	//Open Preview to the Side - ctrl+k v (Mac: cmd+k shift+v)
-	// g.Visualization(graph, "OnlyIds")
-	// g.VisualizationNodeInfo(stringIDToNodeInfo, graph, "IDInfo")
+	// x := w.Walk(graph1, graph1.Node(0), nil)
+	// pageranking := network.PageRank(graph1, 0.85, 0.00001)
 }
