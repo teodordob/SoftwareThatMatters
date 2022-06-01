fc471887d2e02d6a2b95c18befb7dae6d2440fd2
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-06-01 23:25:40 +0200

    Implemented removal of stale edges

--- main.go
+++ main.go
@@ -27,8 +27,11 @@ func main() {
 	graph1, _, _, nodeMap, _ = g.CreateGraph("data/input/test_data.json", true)
 	// This stores whether the package existed in the specified time range
 	withinInterval := make(map[int64]bool, len(nodeMap))
-	// This keeps track of which nodes we visited
+	// This keeps track of which edges we've visited
 	traversed := make([][]bool, len(nodeMap))
+	for from := range traversed {
+		traversed[from] = make([]bool, 0, len(nodeMap))
+	}
 	nodes := graph1.Nodes()
 	for nodes.Next() {
 		n := nodes.Node()
@@ -38,7 +41,7 @@ func main() {
 			withinInterval[id] = true
 		}
 	}
-	// TODO: Remove edges that were not visited
+	// TODO: Discuss if we should just leave packages free-floating if they haven't been visited even once
 	w := traverse.DepthFirst{
 		Traverse: func(e graph.Edge) bool { // The dependent / parent node
 			var traverse bool
@@ -55,14 +58,8 @@ func main() {
 			return traverse
 		},
 	}
-	nodes = graph1.Nodes()
-	for nodes.Next() {
-		n := nodes.Node()
-		if withinInterval[n.ID()] { // We'll only consider traversing this subtree if its root was in the specified time interval
-			_ = w.Walk(graph1, n, nil) // Continue walking this node we've visited everything we're allowed to according to Traverse
-			w.Reset()                  // Clean up for the next iteration
-		}
-	}
+
+	traverseAndRemove(nodes, graph1, withinInterval, w, traversed)
 
 	_ = network.PageRank(graph1, 0.85, 0.00001)
 	//Uncomment this to create the visualization and use these commands in the dot file
@@ -70,4 +67,23 @@ func main() {
 	//Open Preview to the Side - ctrl+k v (Mac: cmd+k shift+v)
 	// g.Visualization(graph, "OnlyIds")
 	// g.VisualizationNodeInfo(stringIDToNodeInfo, graph, "IDInfo")
+}
+
+func traverseAndRemove(nodes graph.Nodes, graph1 *simple.DirectedGraph, withinInterval map[int64]bool, w traverse.DepthFirst, traversed [][]bool) {
+	nodes = graph1.Nodes()
+	for nodes.Next() {
+		n := nodes.Node()
+		if withinInterval[n.ID()] { // We'll only consider traversing this subtree if its root was within the specified time interval
+			_ = w.Walk(graph1, n, nil) // Continue walking this subtree until we've visited everything we're allowed to according to Traverse
+			w.Reset()                  // Clean up for the next iteration
+		}
+	}
+
+	for from := range traversed {
+		for to, val := range traversed[from] {
+			if !val { // If this potential edge wasn't touched at all, remove it
+				graph1.RemoveEdge(int64(from), int64(to)) // Leaves unconnected nodes free-floating; ignores non-existent edges
+			}
+		}
+	}
 }
