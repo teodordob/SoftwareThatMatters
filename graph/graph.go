c225fc08079f98f9393c429de0ff1b64ba349e24
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-06-09 12:16:25 +0200

    Added code to get latest dependencies of a node

--- graph/graph.go
+++ graph/graph.go
@@ -595,6 +595,60 @@ func GetTransitiveDependenciesNode(g *simple.DirectedGraph, nodeMap map[int64]No
 	return &result
 }
 
+// Get the latest dependencies matching the node's version constraints. If you want this within a specific time frame, use filterNode first
+func GetLatestTransitiveDependenciesNode(g *simple.DirectedGraph, nodeMap map[int64]NodeInfo, hashMap map[uint64]int64, stringId string) *[]NodeInfo {
+	var rootNode NodeInfo
+	allDeps := GetLatestTransitiveDependenciesNode(g, nodeMap, hashMap, stringId)
+	result := make([]NodeInfo, 0, len(*allDeps)/2)
+	if len(*allDeps) > 1 {
+		rootNode = (*allDeps)[0]
+	} else {
+		return &result // No-op if no dependencies were found for whatever reason
+	}
+
+	newestPackageVersion := make(map[uint32]NodeInfo, len(*allDeps)/2)
+
+	result = append(result, rootNode)
+
+	// This for loop does the actual filtering
+	for _, current := range *allDeps {
+
+		if current.id == rootNode.id {
+			continue
+		}
+
+		hash := hashPackageName(current.Name)
+		currentDate, err := time.Parse(time.RFC3339, current.Timestamp)
+		if err != nil {
+			continue
+		}
+		if latest, ok := newestPackageVersion[hash]; ok {
+			latestDate, err := time.Parse(time.RFC3339, latest.Timestamp)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			} else if currentDate.After(latestDate) { // If the key exists, and current date is later than the one stored
+				newestPackageVersion[hash] = current // Set to the current package
+			} else if currentDate.Equal(latestDate) { // If the dates are somehow equal, compare version numbers
+				currentversion, _ := semver.NewVersion(current.Version)
+				latestVersion, _ := semver.NewVersion(latest.Version)
+
+				if currentversion.GreaterThan(latestVersion) {
+					newestPackageVersion[hash] = current
+				}
+			}
+		} else { // If the key doesn't exist yet
+			newestPackageVersion[hash] = current
+		}
+	}
+
+	for _, v := range newestPackageVersion { // Add all latest package versions to the result
+		result = append(result, v)
+	}
+
+	return &result
+}
+
 // This uses the sparse page rank algorithm to find the Page ranks of all nodes
 func PageRank(graph *simple.DirectedGraph) map[int64]float64 {
 	pr := network.PageRankSparse(graph, 0.85, 0.01)
