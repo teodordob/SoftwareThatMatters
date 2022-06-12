b7efca0cecd9bd6a795473a182e3df56d5c88981
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-06-12 22:54:47 +0200

    Included gonum license

--- customgraph/directedgraph.go
+++ customgraph/directedgraph.go
@@ -1,3 +1,7 @@
+// Modified simple.Graph code
+// Original Copyright ©2014 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the GONUM-LICENSE file.
 package customgraph
 
 import (
