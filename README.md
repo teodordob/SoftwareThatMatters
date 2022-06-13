70f127b9b0e0b0ec629cf11623751cedfde1f5c2
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-06-13 16:45:20 +0200

    Added documentation for code

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # SoftwareThatMatters
 
-Software that matters analysis and code
+## Instructions
 
-To set up go dependencies, run the following in the root directory (optional, since go should download deps automatically):
+This document will help one reproduce the results mentioned in *Analyzing the Criticality of NPM Packages Through a Time-Dependent Dependency Graph*
+
+To set up go dependencies, run the following in the root directory (optional, since go should download deps automatically) :
 
 ```sh
 go mod download
@@ -11,34 +13,28 @@ go mod download
 To ingest the data, run the following in the root directory:
 
 1. First run:
-   - On Windows (requires wsl with some unix distro installed and with `wget` and `sed`):
+   - On Windows systems (this uses `wsl`):
+      - All packages: `./init.ps1`
+      - If you want only n packages (example with n = 10000): `./init.ps1 10000`
 
-    ```sh
-    ./init.ps1
-    ```
-
-    (If you want only n packages: [example with n = 10000])
-
-    ```sh
-    ./init.ps1 10000
-    ```
-
-   - On unix systems (requires `wget` and `sed`):
-
-   ```sh
-   ./init.sh
-   ```
-
-    (If you want only n packages: [example with n = 10000])
-
-    ```sh
-    ./init.sh 10000
-    ```
+   - On unix systems (requires `wget`):
+      - All packages: `./init.sh`
+      - If you want only n packages (example with n = 10000): `./init.sh 10000`
 
 2. Then run the following:
 
     ```sh
-    go run .
+    go run . ingest
     ```
 
-After running these commands, you'll end up with a file containing the transformed dependencies (`streamedout-merged.json`). This file can then be used to generate a dependency graph.
+After running these commands, you'll end up with a file containing the transformed dependencies (`out-merged.json`). This file can then be used to generate a dependency graph and interactively explore it.
+
+To generate a graph from the acquired data and then interactively explore it (after the first two tasks), run the following:
+
+```sh
+go run . ingest
+```
+
+### License
+
+The code's main license can be found in `LICENSE`. It also re-uses some modified gonum code, for which the license can be found in GONUM_LICENSE
--- main.go
+++ main.go
@@ -1,75 +1,11 @@
+/*
+Copyright © 2022 A.J.M Brands A.J.M.Brands@student.tudelft.nl
+
+*/
 package main
 
-import (
-	"runtime/debug"
-
-	"github.com/AJMBrands/SoftwareThatMatters/cmd"
-)
+import "github.com/AJMBrands/SoftwareThatMatters/cmd"
 
 func main() {
-	debug.SetGCPercent(5)
-	//TODO: Move to graph.go; Integrate nicely with cli
-	// To use the cli: go run main.go start.
 	cmd.Execute()
-	// var wg sync.WaitGroup
-
-	// go func() {
-	// 	fmt.Println(http.ListenAndServe("localhost:6060", nil))
-	// }()
-
-	// wg.Add(1)
-
-	// graph1, _, idToNodeInfo, _ := graph.CreateGraph("data/input/processed-100k.json", false)
-
-	// pr := graph.PageRank(graph1)
-	// keys := make([]int64, 0, len(pr))
-	// for k := range pr {
-	// 	keys = append(keys, k)
-	// }
-
-	// sort.SliceStable(keys, func(i, j int) bool {
-	// 	return pr[keys[i]] > pr[keys[j]]
-	// })
-
-	// count := 10
-	// for i := 0; i < count; i++ {
-	// 	fmt.Printf("The n-th (n = %d) highest-ranked node (%v) has rank %f \n", i, idToNodeInfo[keys[i]], pr[keys[i]])
-	// }
-
-	// graph.VisualizationNodeInfo(idToNodeInfo, graph1, "IDInfo")
-	// wg.Wait()
-	// pr := network.PageRankSparse(graph1, 0.85, 0.001)
-
-	//duration := 365 * 24 * time.Hour
-	//beginTime, _ := time.Parse(time.RFC3339, "2021-01-01T00:00:00Z01:00")
-	//endTime := beginTime.Add(duration)
-	////
-	//var nodeMap map[int64]g.NodeInfo
-	//var graph1 *simple.DirectedGraph
-	//var stringIDToNodeInfo map[string]g.NodeInfo
-	//graph1, _, stringIDToNodeInfo, nodeMap, _ = g.CreateGraph("data/input/processed-10k.json", false)
-	//x := g.GetTransitiveDependenciesNode(graph1, nodeMap, stringIDToNodeInfo, "1221-1.0.0")
-	//fmt.Println(x)
-	////
-	//g.FilterGraph(graph1, nodeMap, beginTime, endTime)
-	//fmt.Println(graph1)
-	//_ = network.PageRank(graph1, 0.85, 0.00001)
-
-	//var nodeMap map[int64]g.NodeInfo
-	//var stringMap map[string]g.NodeInfo
-	//var graph1 *simple.DirectedGraph
-	//graph1, _, stringMap, nodeMap, _ = g.CreateGraph("data/input/test_data.json", true)
-	//g.FilterGraph(graph1, nodeMap, beginTime, endTime)
-	//
-	//g.FilterNode(graph1, nodeMap, stringMap, "A-1.0.0", beginTime, endTime)
-	//_ = network.PageRank(graph1, 0.85, 0.00001)
-
-	//g.FilterNode(graph1, nodeMap, stringMap, "A-1.0.0", beginTime, endTime)
-	//g.GetTransitiveDependenciesNode(graph1, nodeMap, stringMap, "A-1.0.0")
-	//_ = network.PageRank(graph1, 0.85, 0.00001)
-
-	//Uncomment this to create the visualization and use these commands in the dot file
-	//Toggle Preview - ctrl+shift+v (Mac: cmd+shift+v)
-	//Open Preview to the Side - ctrl+k v (Mac: cmd+k shift+v)
-	//graph.Visualization(graph1, "OnlyIds")
 }
