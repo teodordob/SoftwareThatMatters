d2a6c669ae8f8ba373fdc5cf6e6a1978df77ea7a
Anna Brands <A.J.M.Brands@student.tudelft.nl>
2022-06-16 13:36:35 +0200

    Add correct filtering options

--- cmd/start.go
+++ cmd/start.go
@@ -88,6 +88,7 @@ func start() {
 				"Find the latest dependencies of a package between two timestamps",
 				"Find the n most used packages by PageRank between two time stamps",
 				"Find the n most used packages by PageRank (without considering time)",
+				"Find the n most used packages by node betweenness",
 				"Find the n most used packages by node betweenness (without considering time)",
 				"Quit",
 			},
@@ -169,6 +170,13 @@ func start() {
 			}
 		case 6:
 			fmt.Println("This should find the n most used packages (by node betweenness)")
+			beginTime := generateAndRunDatePrompt("Please input the beginning date of the interval (DD-MM-YYYY)")
+			endTime := generateAndRunDatePrompt("Please input the end date of the interval (DD-MM-YYYY)")
+			fmt.Println("Getting the latest dependencies for packages. This will take a while")
+			t1 := time.Now().Unix()
+			g.FilterLatestDepsGraph(graph, idToNodeInfo, hashMap, beginTime, endTime)
+			t2 := time.Now().Unix()
+			fmt.Printf("Graph filtering took %d seconds\n", t2-t1)
 			fmt.Println("Running node betweenness algorithm")
 			b := g.Betweenness(graph)
 			keys := make([]int64, 0, len(b))
@@ -185,6 +193,23 @@ func start() {
 				fmt.Printf("The number %d highest-ranked node (%v) has rank %f \n", i, idToNodeInfo[keys[i]], b[keys[i]])
 			}
 		case 7:
+			fmt.Println("This should find the n most used packages (by node betweenness)")
+			fmt.Println("Running node betweenness algorithm")
+			b := g.Betweenness(graph)
+			keys := make([]int64, 0, len(b))
+			for k := range b {
+				keys = append(keys, k)
+			}
+
+			sort.SliceStable(keys, func(i, j int) bool {
+				return b[keys[i]] > b[keys[j]]
+			})
+
+			count := generateAndRunNumberPrompt("Please select the number (n > 0) of highest-ranked packages you wish to see")
+			for i := 0; i < count; i++ {
+				fmt.Printf("The number %d highest-ranked node (%v) has rank %f \n", i, idToNodeInfo[keys[i]], b[keys[i]])
+			}
+		case 8:
 			fmt.Println("Stopping the program...")
 			stop = true
 		}
